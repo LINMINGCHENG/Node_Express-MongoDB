bba899f02acf9a293c2879e27b6fadb7d924fdf5
McJohnsonLin <48139258+LINMINGCHENG@users.noreply.github.com>
2020-08-03 13:15:14 -0400

    added

---
+++ controllers/bootcamps.js
@@ -0,0 +1,40 @@
+exports.getBootcamps= (req,res,next) => {
+    res.status(200).json({
+        sucess:true,
+        msg:'Show all bootcamps'});
+}
+
+exports.getBootcamp= (req,res,next)=>{
+  res.status(200).json({
+      success: true,
+      msg: `show bootcamp ${req.params.id}`
+  })
+}
+
+
+exports.createBootcamp = (req, res, next) => {
+   res.status(200).json({
+       success: true,
+       msg: "create new bootcamp"
+   })
+}
+exports.updateBootcamp = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: `update bootcamp ${req.params.id}`
+    })
+}
+
+exports.deleteBootcamp = (req, res, next) => {
+  res.status(200).json({
+      success: true,
+      msg: `delete bootcamp ${req.params.id}`
+  })
+}
+
+
+
+
+
+
+
