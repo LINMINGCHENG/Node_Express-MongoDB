fbf74def5c689fba1e911f2ac47642d08c8a021a
McJohnsonLin <48139258+LINMINGCHENG@users.noreply.github.com>
2020-08-03 20:13:59 -0400

    added

--- config/db.js
+++ config/db.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
-const connection = async () =>{
-   const conn = await mongoose.connect();
+const connectDB = async () => {
+   const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify:false,
+      useUnifiedTopology:true
+   });
+   console.log(`Mongodb Connected: ${conn.connection.host}`);
 }
+
+module.exports= connectDB;
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const dotenv =  require('dotenv');
 const morgan = require("morgan");
+const connectDB =require('./config/db')
+//load env vars
+dotenv.config({path:'./config/config.env'})
+
+//connet to detabase
+connectDB();
 
 //Route
 const bootcamps =require('./routes/bootcamps');
 
-
-//load env vars
-dotenv.config({path:'./config/config.env'})
 
 const app =express();
 
@@ -30,4 +33,5 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server runing in ${process.env.NODE_ENV} mode on ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server runing in ${process.env.NODE_ENV} mode on ${PORT}`));
+//Handle unhandled promises 
